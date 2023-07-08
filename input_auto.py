e92014c90de4870827c8c6338430d08796d2ac15
Jo Thomas <jothomas1996@gmail.com>
2023-07-08 12:20:28 +0530

    design fixes

--- input_auto.py
+++ input_auto.py
@@ -26,7 +26,7 @@ class InputAutoGUI:
         
     def setup_ui(self):
         source_frame = tk.Frame(self.root)
-        source_frame.pack(padx=10, pady=10, anchor=tk.W)
+        source_frame.pack(padx=10, pady=10, anchor=tk.W, fill=tk.X)
 
         self.setup_source_section(source_frame)
         
@@ -35,17 +35,51 @@ class InputAutoGUI:
 
         self.record_button = tk.Button(button_frame, text="Record", command=self.toggle_recording, relief=tk.RAISED, compound=tk.LEFT)
         self.record_button.pack(side=tk.LEFT, padx=5)
-        self.set_record_button_icon("record")
 
         self.clear_button = tk.Button(button_frame, text="Clear", command=self.clear_actions, relief=tk.RAISED, compound=tk.LEFT)
         self.clear_button.pack(side=tk.LEFT, padx=5)
 
+        self.set_record_button_icon("record")
+
         self.text_area = tk.Text(self.root, height=10, width=50)
-        self.text_area.pack(padx=10, pady=5, anchor=tk.W)
+        self.text_area.pack(padx=10, pady=5, anchor=tk.W, fill=tk.X)
+
+        # Add Loop section
+        loop_section = tk.LabelFrame(self.root, text="Loop", padx=10, pady=10)
+        loop_section.pack(padx=10, pady=10, anchor=tk.W, fill=tk.X)
+
+        # Row 1: Numeric Input and Loop Button
+        row1 = tk.Frame(loop_section)
+        row1.pack(padx=10, pady=5, anchor=tk.W)
+
+        self.count_var = tk.IntVar()
+        count_entry = tk.Spinbox(row1, from_=1, to=50, width=3, textvariable=self.count_var)
+        count_entry.pack(side=tk.LEFT, padx=5)
+
+        loop_button = tk.Button(row1, text="Loop", command=self.start_loop)
+        loop_button.pack(side=tk.LEFT, padx=5)
+
+        # Row 2: Current Loop
+        row2 = tk.Frame(loop_section)
+        row2.pack(padx=10, pady=2, anchor=tk.W)
+        self.current_loop_var = tk.IntVar()
+        current_loop_label = tk.Label(row2, text="Current loop: ")
+        current_loop_label.pack(side=tk.LEFT)
+        current_loop_value = tk.Label(row2, textvariable=self.current_loop_var)
+        current_loop_value.pack(side=tk.LEFT)
+
+        # Row 3: Remaining Loops
+        row3 = tk.Frame(loop_section)
+        row3.pack(padx=10, pady=2, anchor=tk.W)
+        self.remaining_loops_var = tk.IntVar()
+        remaining_loops_label = tk.Label(row3, text="Remaining loops: ")
+        remaining_loops_label.pack(side=tk.LEFT)
+        remaining_loops_value = tk.Label(row3, textvariable=self.remaining_loops_var)
+        remaining_loops_value.pack(side=tk.LEFT)
         
     def setup_source_section(self, frame):
         source_frame = tk.LabelFrame(frame, text="Source", padx=10, pady=10)
-        source_frame.pack(padx=0, pady=0, anchor=tk.W)
+        source_frame.pack(padx=0, pady=0, anchor=tk.W, fill=tk.X)
         
         for index, (device, value) in enumerate(self.available_devices.items()):
             checkbox = tk.Checkbutton(source_frame, text=device, variable=value, onvalue=True, offvalue=False)
@@ -140,6 +174,14 @@ class InputAutoGUI:
         self.text_area.delete('1.0', tk.END)
         for action in self.recorded_actions:
             self.text_area.insert(tk.END, action + '\n')
+
+        # Update Current loop and Remaining loops values
+        self.current_loop_var.set(current_loop_value)
+        self.remaining_loops_var.set(remaining_loops_value)
+
+    def start_loop(self):
+        count = self.count_var.get()
+        # Perform loop operation using the 'count' value
 
     def cleanup(self):
         if self.is_recording:
